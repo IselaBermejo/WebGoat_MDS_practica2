d527408baf51cf85cf22c99d2c4aa7710cbbce3a
IselaBermejo <iselabs2001@gmail.com>
2023-04-14 11:13:34 +0200

    Modificación Vulnerabilidad Blocker

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -56,8 +56,8 @@ public class Assignment5 extends AssignmentEndpoint {
     }
 
     String query = "select password from challenge_users where userid = ? and password = ?";
-    try (var connection = dataSource.getConnection()) {
-      PreparedStatement statement = connection.prepareStatement(query);
+    try (var connection = dataSource.getConnection();
+         PreparedStatement statement = connection.prepareStatement(query)){
       statement.setString(1, username_login);
       statement.setString(2, password_login);
 
